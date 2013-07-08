c16cea149516484776ce419b9bf19cbdf79f86fb
Tom August <tomaug@ceh.ac.uk>
2013-07-08 17:33:49 +0100

    0.1.0 - Major update. Functions seperated and improved. sparta() no longer supported.

--- R/is.gridcell.wellsampled.R
+++
@@ -1,7 +0,0 @@
-is.gridcell.wellsampled <-
-function(CellID, n=3){
-  # modified version of filter.gridcells()
-  #takes the dataframe and returns the rownumbers of the dataset identifying well-sampled the gridcells
-  nyrs <- table(CellID)
-  return(CellID %in% names(nyrs)[nyrs >= n])
-}
