381ca8d5916fc3f979380eb4a238bb2f2ff88db9
ThierryO <ggplot2@muscardinus.be>
2013-09-23 15:32:19 +0200

    Update fit_maes_trend.r
    Changing the way p-values are calculated to get more correct two-sided p-values.
    Some parts are rewritten to make to code a bit more elegant.
    Adding some white space for readability.

--- R/fit_maes_trend.r
+++ R/fit_maes_trend.r
@@ -1,4 +1,4 @@
-fit_maes_trend<-function(records, time_periods=NULL, min_sp=5){
+fit_maes_trend <- function(records, time_periods = NULL, min_sp = 5){
   
   # fits the method described in Maes et al 2012 (Biol Cons 145: 258-266)
   # I checked this by comparing the results against those in Maes et al SI
@@ -7,49 +7,53 @@ fit_maes_trend<-function(records, time_periods=NULL, min_sp=5){
   require(reshape2)
   
   if(is.null(time_periods)) stop('No time periods supplied to fit_maes_trend')
-  
+
   # Create a vector of all the years to include
-  for (i in 1:length(row.names(time_periods))) {
-    run<-time_periods[i,1]:time_periods[i,2]
-    if (exists('all_years')){
-      all_years <- c(all_years,run)
-    }else{
-      all_years <- run
-    }
-  }
+  all_years <- unlist(
+    apply(
+      time_periods, 
+      1, 
+      function(x){
+        list(x[1]:x[2])
+      }
+    )
+  )
   
   
   # Subset data to years needed
-  records<-records[records$tpnew %in% all_years,]
-  
-  splityr <- mean(c(max(time_periods[1,]),min(time_periods[2,])))
+  records <- records[records$tpnew %in% all_years, ]
+  splityr <- mean(c(time_periods$end[1], time_periods$start[2]))
     
   # which time period is each record in?
-  records$tp[records$tpnew<splityr & records$tpnew<splityr]<-1
-  records$tp[records$tpnew>=splityr & records$tpnew>=splityr]<-2
+  records$tp <- cut(records$tpnew,
+    sort(
+      c(0, splityr, Inf)
+    )
+  )
     
-  records<-records[!is.na(records$tp),]
+  records <- records[!is.na(records$tp), ]
+  
   # convert the records into a 3D array
-  rc <- acast(records, Species ~ Site ~ tp, fun=occurrence, value.var=2)
+  rc <- acast(records, Species ~ Site ~ tp, fun = occurrence, value.var = 2)
 
   # what is the observed species richness in each cell in each year
   rc1 <- apply(rc, c(2,3), sum)
-  
+
   # which sites are well-sampled? (defined as having at least min_sp species in BOTH time periods)
-  well_sampled <- as.numeric(dimnames(rc1)[[1]][apply(rc1, 1, function(x) all(x>=min_sp))])
+  well_sampled <- which(apply(rc1 >= min_sp, 1, all))
   
   # look at just the data for these well-sampled cells
-  rc2 <- rc[,dimnames(rc)[[2]] %in% well_sampled,]
+  rc2 <- rc[, well_sampled, ]
   
   # how many sites for each species in each time period?
   rc3 <- apply(rc2, c(1,3), sum)
-  
+
   # calculate the relative distribution in each time period
   # the denominator is the total number of unqiue site-species combos within the well-sampled set (for each period)
   rd <- apply(rc3, 2, function(x) x/sum(x))
-  
+
   # the trend is the % difference in the two relative distributions
-  trend <- 100 * (rd[,2]-rd[,1])/rd[,1]    
+  trend <- 100 * (rd[, 2] - rd[, 1]) / rd[, 1]
   
   # we can assess the significance of this method as follows
   # first we assume the distribution of each species is equal among poorly-sampled and well-sampled sites
@@ -58,16 +62,60 @@ fit_maes_trend<-function(records, time_periods=NULL, min_sp=5){
   # estimated number of successes is defined here as nsr2 (number of sites recorded in t2)
   # where the number of trials = nS[2]
   
-  pval <- mapply(FUN=pbinom, q=rc3[,2], prob=rd[,1], MoreArgs=list(size=sum(rc3[,2])))
-  
-  #these are one-tailed: convert them to one-tailed
-  pval <- one_to_two_tail(pval)
-  
+  n <- colSums(rc3)
+  #confidence intervals under the alternative hypothesis
+  CI <- t(apply(rd, 1, function(x){
+    quantile(
+      na.omit(#ignore trends where the species is absent in both time periods
+        apply(
+          #simulate under the alternative hypothesis
+          matrix(
+            rbinom(2e3, size = n, prob = x) / n,
+            nrow = 2
+          ),
+          2,
+          #calculate simulated trend under the alternative hypothesis
+          function(y){
+            (y[2] - y[1]) / y[1]
+          }
+        )
+      ),
+      prob = c(0.025, 0.975)
+    )
+  }))
 
-  Maes <- data.frame(gridcells1=rc3[,1], relDist1=rd[,1], gridcells2=rc3[,2], relDist2=rd[,1], change=trend, pval=pval) 
+  #p-values under the null hypothesis
+  p.values <- t(apply(rd, 1, function(x){
+    trend <- (x[2] - x[1]) / x[1]
+    trend.null <- na.omit(apply(
+      #simulate under the null hypothesis x[1] == x[2]
+      matrix(
+        rbinom(2e3, size = n, prob = x[1]) / n,
+        nrow = 2
+      ),
+      2,
+      #calculate simulated trend under the null hypothesis
+      function(y){
+        (y[2] - y[1]) / y[1]
+      }
+    ))
+    c(
+      trend = unname(trend),
+      p.negative.trend.one.sided = mean(trend.null <= trend),
+      p.positive.trend.one.sided = mean(trend.null >= trend),
+      p.absolute.trend.two.sided = mean(abs(trend.null) >= abs(trend))
+    )
+  }))
+
+  Maes <- data.frame(
+    gridcells1 = rc3[, 1], 
+    relDist1 = rd[, 1], 
+    gridcells2 = rc3[, 2], 
+    relDist2 = rd[, 1], 
+    change = p.values[, "trend"], 
+    pval = p.values[, "p.absolute.trend.two.sided"]
+  )
   attr(Maes, 'GridCellSums') <- colSums(rc3)
   attr(Maes, 'wellsampled') <- length(well_sampled)
   return(Maes)
 }
-
-one_to_two_tail <- function(p) ifelse(p<0.5, p*2, (1-p)*2)
