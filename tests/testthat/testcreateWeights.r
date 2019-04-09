023d9445e9a018a397667feecec915696f59e885
AugustT <tomaug@ceh.ac.uk>
2019-04-09 11:18:37 +0100

    Update createWeights and some test scripts so that the test outputs work better. Note frescalo is no longer tested because carbon black blocks the .exe

--- tests/testthat/testcreateWeights.r
+++ tests/testthat/testcreateWeights.r
@@ -15,27 +15,32 @@ dist <- melt(dist)
 sim <- data.frame(SQ_SQUARE = sites)
 sim[,2:11] <- runif(n*10, min=0, max=10)
 
-temp <- capture.output(weights <- createWeights(dist, sim, dist_sub = 5, sim_sub = 3))
+temp <- capture.output(weights <- createWeights(dist, sim, dist_sub = 5, sim_sub = 3, verbose = FALSE))
 
 test_that("Test errors and warnings", {
   
-  expect_warning(createWeights(dist, sim[1:5,], dist_sub = 5, sim_sub = 3),
+  expect_warning(createWeights(dist, sim[1:5,], dist_sub = 5, sim_sub = 3,
+                               verbose = FALSE),
                  "The following sites were in only one of 'attributes' and 'distances' and so have been excluded from the weights file")
-  expect_error(createWeights(head(dist, -1), sim, dist_sub = 5, sim_sub = 3),
+  expect_error(createWeights(head(dist, -1), sim, dist_sub = 5, sim_sub = 3,
+                             verbose = FALSE),
                'dist table does not include all possible combinations of sites')  
-  expect_error(createWeights(dist, sim, dist_sub = 5, sim_sub = 5),
+  expect_error(createWeights(dist, sim, dist_sub = 5, sim_sub = 5, verbose = FALSE),
                "dist_sub' cannot be smaller than or equal to 'sim_sub'")  
-  expect_error(createWeights(dist, sim, dist_sub = 5, sim_sub = 10),
+  expect_error(createWeights(dist, sim, dist_sub = 5, sim_sub = 10, verbose = FALSE),
                "dist_sub' cannot be smaller than or equal to 'sim_sub'") 
-  expect_error(createWeights(TRUE, sim, dist_sub = 5, sim_sub = 3),
+  expect_error(createWeights(TRUE, sim, dist_sub = 5, sim_sub = 3, verbose = FALSE),
                "dist must be a data.frame") 
-  expect_error(createWeights(dist, 'Tom', dist_sub = 5, sim_sub = 3),
+  expect_error(createWeights(dist, 'Tom', dist_sub = 5, sim_sub = 3, verbose = FALSE),
                "sim must be a data.frame") 
-  expect_error(createWeights(cbind(dist, 1:nrow(dist)), sim, dist_sub = 5, sim_sub = 3),
+  expect_error(createWeights(cbind(dist, 1:nrow(dist)), sim, dist_sub = 5,
+                             sim_sub = 3, verbose = FALSE),
                "dist must have three columns")
-  expect_error(createWeights(cbind(dist[,1:2], as.character(dist[,3])), sim, dist_sub = 5, sim_sub = 3),
+  expect_error(createWeights(cbind(dist[,1:2], as.character(dist[,3])), sim,
+                             dist_sub = 5, sim_sub = 3, verbose = FALSE),
                "the value column in dist must be an integer or numeric") 
-  expect_error(createWeights(dist, cbind(sim[1:10], as.character(sim[,11])), dist_sub = 5, sim_sub = 3),
+  expect_error(createWeights(dist, cbind(sim[1:10], as.character(sim[,11])),
+                             dist_sub = 5, sim_sub = 3, verbose = FALSE),
                'the values in sim must be integers or numeric') 
   
 })
