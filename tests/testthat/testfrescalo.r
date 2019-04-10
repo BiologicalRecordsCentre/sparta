9dc6665901c4499a46edc59872041e70eda19e9f
AugustT <tomaug@ceh.ac.uk>
2019-04-10 15:43:09 +0100

    Add testing of plotting and very high values of phi which trigger a re-run

--- tests/testthat/testfrescalo.r
+++ tests/testthat/testfrescalo.r
@@ -1,8 +1,7 @@
 context("Test frescalo")
 
 ### Frescalo testing is currently skipped if not on a ###
-### windows machine, or is libcurl is not supported. I###
-### need to expand testing to linux if possible.      ###
+### windows machine, or is libcurl is not supported.  ###
 
 # Create data
 n <- 1500 #size of dataset
@@ -21,7 +20,7 @@ rDates <- first + (runif(nSamples)*dt)
 taxa <- sample(letters, size = n, TRUE)
 
 # three sites are visited randomly
-site <- sample(paste('A', 1:nSites, sep=''), size = n, TRUE)
+site <- sample(paste('SK', 1:nSites, sep=''), size = n, TRUE)
 
 # the date of visit is selected at random from those created earlier
 time_period <- sample(rDates, size = n, TRUE)
@@ -161,7 +160,7 @@ test_that("Runs without error", {
   dir.create(temp)
   sink(file.path(temp, 'null'))
   fres_try <- try(frescalo(Data = df1,
-                           phi = 0.01,
+                           phi = 0.51,
                            Fres_weights = weights,
                            frespath = frespath,
                            time_periods = data.frame(start=c(1980,1990),end=c(1989,1999)),
@@ -181,13 +180,17 @@ test_that("Runs without error", {
                 "freq" %in% names(fres_try) &
                 "log" %in% names(fres_try) &
                 "lm_stats" %in% names(fres_try))
+
+})
+
+
+test_that("Test plotting", {
   
   # test plotting
   temp <- tempfile(pattern = 'dir')
   dir.create(temp)
   sink(file.path(temp, 'null'))
   fres_try <- try(frescalo(Data = df1,
-                           phi = 0.01,
                            Fres_weights = weights,
                            frespath = frespath,
                            time_periods = data.frame(start=c(1980,1990),end=c(1989,1999)),
@@ -208,4 +211,68 @@ test_that("Runs without error", {
                 "freq" %in% names(fres_try) &
                 "log" %in% names(fres_try) &
                 "lm_stats" %in% names(fres_try))
+})
+
+
+# Create data
+n <- 15000 #size of dataset
+nyr <- 20 # number of years in data
+nSamples <- 100 # set number of dates
+nSites <- 50 # set number of sites
+set.seed(125)
+
+# Create somes dates
+first <- as.Date(strptime("1980/01/01", "%Y/%m/%d")) 
+last <- as.Date(strptime(paste(1980+(nyr-1),"/12/31", sep=''), "%Y/%m/%d")) 
+dt <- last-first 
+rDates <- first + (runif(nSamples)*dt)
+
+# taxa are set as random letters
+taxa <- sample(letters, size = n, TRUE)
+
+# three sites are visited randomly
+site <- sample(paste('A', 1:nSites, sep=''), size = n, TRUE)
+
+# the date of visit is selected at random from those created earlier
+time_period <- sample(rDates, size = n, TRUE)
+
+df1 <- data.frame(taxa = taxa,
+                  site = site,
+                  year = as.numeric(format(time_period, '%Y')),
+                  startdate = time_period,
+                  enddate = time_period + 500)
+
+allsites <- sort(unique(site))
+
+weights <- merge(allsites, allsites)
+weights$W <- runif(n = nrow(weights), min = 0, max = 1)
+
+test_that("Runs high value of phi", {
+  
+  # test a very low value of phi
+  temp <- tempfile(pattern = 'dir')
+  dir.create(temp)
+  sink(file.path(temp, 'null'))
+  fres_try <- try(frescalo(Data = df1,
+                           Fres_weights = weights,
+                           phi = NULL,
+                           frespath = frespath,
+                           time_periods = data.frame(start=c(1980,1990),end=c(1989,1999)),
+                           site_col = 'site',
+                           sp_col = 'taxa',
+                           year = 'year',
+                           sinkdir = temp),
+                  silent=TRUE
+  )
+  sink()
+  unlink(temp, recursive = TRUE)
+  
+  expect_equal(class(fres_try), "frescalo")
+  expect_true("paths" %in% names(fres_try) &
+                "trend" %in% names(fres_try) &
+                "stat" %in% names(fres_try) &
+                "freq" %in% names(fres_try) &
+                "log" %in% names(fres_try) &
+                "lm_stats" %in% names(fres_try))
+  
 })
