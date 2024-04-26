8597e361c3a0721e85020b18eb3bf4e60752d0f4
DylanCarbone <dylcar@ceh.ac.uk>
2024-04-26 14:50:34 +0100

    removed package I added that was not needed

--- R/detect_jags.r
+++
@@ -1,5 +0,0 @@
-# Internal function to detect JAGS installation.
-#' @importFrom runjags testjags
-detect_jags <- function(){
-    return(suppressWarnings(runjags::testjags(silent = TRUE)$JAGS.found))
-}
