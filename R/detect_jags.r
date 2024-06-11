4193ded7860fecc9ecfe1d263a5d8bdf76321ea4
DylanCarbone <dylcar@ceh.ac.uk>
2024-06-11 17:08:34 +0100

    fix to detect jags break;

--- R/detect_jags.r
+++ R/detect_jags.r
@@ -1,5 +1,6 @@
-# Internal function to detect JAGS installation.
+# Internal function to detect JAGS installation. Note rjags is needed to detect the JAGS installation
 #' @importFrom runjags testjags
+#' @import rjags
 #' 
 detect_jags <- function(){
         return(suppressWarnings(runjags::testjags(silent = TRUE)$JAGS.found))
